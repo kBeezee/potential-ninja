4445014d52645cdeceacab05864f8e8dc984470e
Kyle <kboxer84@gmail.com>
2015-06-14 19:08:55 -0500

    now with mediocre jumping!

--- start.py
+++ start.py
@@ -12,9 +12,12 @@ import sClasses
 pygame.init()
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 24)
+pygame.key.set_repeat(500, 30)
 
 WINDOWSIZE = (800, 600)
 SPRITESCALE = 13
+TILESCALE = 8
+
 screen = pygame.display.set_mode(WINDOWSIZE, pygame.DOUBLEBUF)
 pygame.display.set_caption("Platforming at its Learningest!")
 
@@ -26,6 +29,7 @@ pcIdle = pygame.sprite.Group()
 pcRun = pygame.sprite.Group()
 pcJumpUp = pygame.sprite.Group()
 pcJumpDown = pygame.sprite.Group()
+
 tiles = pygame.sprite.Group()
 
 def importbackgrounds():
@@ -37,6 +41,7 @@ def importbackgrounds():
     backgrounds.add("assets/bg3/layers/layer-6.png", 15, (WINDOWSIZE))
 
 def importsprites():
+    global Ground
     scale = tuple(numpy.divide(WINDOWSIZE, SPRITESCALE))
     NewSprite = sClasses.Player(scale, 'idle')
     pcIdle.add(NewSprite)
@@ -48,8 +53,8 @@ def importsprites():
     pcJumpDown.add(NewSprite)
 
 def eventhandler(event):
-    global PlayerState, PlayerDirection, speed
-    ##CLOSE WINDOW
+    global PlayerState, PlayerDirection, speed, vspeed, Falling
+    ##CLOSE WINDOWs
     if event.type == pygame.QUIT:
         sys.exit()
 
@@ -59,60 +64,81 @@ def eventhandler(event):
             pygame.quit()
             sys.exit(0)
     ##CLOSE WINDOW
+
     ##PLAYER MOVEMENT
         ##HORIZONTAL MOVEMENT
     if event.type == KEYDOWN and event.key == K_RIGHT:
-        speed += 10
         PlayerState = 'run'
         PlayerDirection = 'right'
     if event.type == KEYUP and event.key == K_RIGHT:
-        speed -= 10
         PlayerState = 'idle'
         PlayerDirection = 'right'
     if event.type == KEYDOWN and event.key == K_LEFT:
-        speed -= 10
         PlayerState = 'run'
         PlayerDirection = 'left'
     if event.type == KEYUP and event.key == K_LEFT:
-        speed += 10
         PlayerState = 'idle'
         PlayerDirection = 'left'
         ##HORIZONTAL MOVEMENT
+
         ##VERTICAL MOVEMENT
-    if pressedkeys[K_SPACE]:
+    if event.type == KEYDOWN and event.key == K_SPACE:
         PlayerState = 'jump up'
     if event.type == KEYUP and event.key == K_SPACE:
-        PlayerState = 'jump down'
         #todo: after collision, PlayerState needs to go back to whatever button is being pressed
+        PlayerState = 'jump down'
+
         ##VERTICAL MOVEMENT
     ##PLAYER MOVEMENT
 
 def renderscreen(PlayerState, PlayerDirection):
     def parallax():
-        global backgrounds, time_ref, speed
+        global backgrounds, time_ref, speed, vspeed
         backgrounds.scroll(speed, 'horizontal')
         t = pygame.time.get_ticks()
         if (t - time_ref) > 60:
             backgrounds.draw(screen)
 
     def spriteanimation(PlayerState, PlayerDirection):
+        global speed, vspeed, Falling
+        if vspeed >= 80 and not Falling:
+            vspeed -= 2
+            Falling = True
+        elif vspeed <= 0:
+            vspeed = 0
+            Falling = False
         if PlayerState == 'run':
-            pcRun.update(PlayerDirection)
+            if PlayerDirection == 'right':
+                speed = 10
+            elif PlayerDirection == 'left':
+                speed = -10
+            if vspeed > 0 and Falling == True:
+                vspeed -= 2
+            pcRun.update(PlayerDirection, vspeed)
             pcRun.draw(screen)
         elif PlayerState == 'jump up':
-            pcJumpUp.update(PlayerDirection)
+            if not Falling:
+                vspeed += 1
+            if Falling:
+                vspeed -= 2
+
+            pcJumpUp.update(PlayerDirection, vspeed)
             pcJumpUp.draw(screen)
         elif PlayerState == 'jump down':
-            pcJumpDown.update(PlayerDirection)
+            Falling = True
+            if vspeed > 0 and Falling == True:
+                vspeed -= 2
+            pcJumpDown.update(PlayerDirection, vspeed)
             pcJumpDown.draw(screen)
-        else:
-            pcIdle.update(PlayerDirection)
+        elif PlayerState == 'idle':
+            speed = 0
+            if vspeed > 0 and Falling == True:
+                vspeed -= 2
+            pcIdle.update(PlayerDirection, vspeed)
             pcIdle.draw(screen)
 
-    screen.blit(blackground, (0, 0))
     parallax()
     spriteanimation(PlayerState, PlayerDirection)
-
     pygame.display.flip()
 
 #init some things
@@ -122,12 +148,15 @@ importsprites()
 #flags and longs etc.
 time_ref = 0
 speed = 0
+vspeed = 0
+Falling = False
 PlayerState = 'idle'
 PlayerDirection = 'right'
 while True:
     #Event Handleing
     for event in pygame.event.get():
         eventhandler(event)
-    #print pygame.mouse.get_pos()
+
+    #Rendering
     renderscreen(PlayerState, PlayerDirection)
 
